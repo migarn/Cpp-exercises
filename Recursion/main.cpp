312ac3ff3007c87ad045c2afb5ab9225cac23eda
migarn <michalgarnczarski@gmail.com>
2020-05-23 17:33:03 +0200

    Recursive summing implemented

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
 
-using namespace std;
+int sum5(int elems[], size_t size)
+{
+    if (size == 0) { return 0; }
+    else { return sum5(elems, size - 1) + elems[size - 1]; }
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    using std::cout;
+    using std::endl;
+    using namespace std::chrono;
+
+    int a1[] = {0,1,2,3,4};
+    constexpr size_t a1Size = sizeof(a1) / sizeof(a1[0]);
+
+    cout << "sum5(10) = " << sum5(a1, a1Size) << endl;
     return 0;
 }
