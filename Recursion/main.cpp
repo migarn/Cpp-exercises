e0dc2c24d98fd4c109ad4a0073517d507b393008
migarn <michalgarnczarski@gmail.com>
2020-05-23 17:51:23 +0200

    Computing nth element of Fibonacci sequence implemented

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -14,6 +14,13 @@ long factR(int n)
     else { return n * factR(n - 1); }
 }
 
+int fibR(int n)
+{
+    if (n <= 0) { return 0; }
+    else if (n == 1) { return 1; }
+    else { return fibR(n - 2) + fibR(n - 1); }
+}
+
 int main()
 {
     using std::cout;
@@ -25,5 +32,17 @@ int main()
 
     cout << "factR(20) = " << factR(20) << endl;
     cout << "sum5(10) = " << sum5(a1, a1Size) << endl;
+
+    high_resolution_clock::time_point start, end;
+
+    int n4Fib = 45;
+    start = high_resolution_clock::now();
+    fibR(n4Fib);
+    end = high_resolution_clock::now();
+
+    cout << "fibR(" << n4Fib << ") took "
+        << duration_cast<duration<double>>(end - start).count()
+        << " seconds.\n" << endl;
+
     return 0;
 }
