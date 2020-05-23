9104df0ed0ab886e5d2cdedba5a530c06c01e768
migarn <michalgarnczarski@gmail.com>
2020-05-23 13:02:18 +0200

    if else structure created

--- ControlStructures/main.cpp
+++ ControlStructures/main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+void fun1()
+{
+    float f1 = 0.1f, f2 = 0.3f, f3 = 0.4f;
+    float f4 = (f1 + f2) + f3;
+    float f5 = f1 + (f2 + f3);
+
+    cout << "f4 = " << f4 << endl;
+    cout << "f5 = " << f5 << endl;
+
+    if (f4 == f5) {
+        cout << "f4 == f5" << endl;
+    } else {
+        cout << "f4 != f5" << endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    fun1();
     return 0;
 }
