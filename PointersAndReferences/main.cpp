c820e4590e19b1b47be473c99ece1c9b0c5dbfc5
migarn <michalgarnczarski@gmail.com>
2020-05-29 19:16:59 +0200

    PointersAndReferences main class implemented

--- PointersAndReferences/main.cpp
+++ PointersAndReferences/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 
-using namespace std;
+void pointersDemo1()
+{
+    int x = 1;
+    int *p1 = $x;
+    int **pp1 = &p1;
+
+    printf("&x = %p, x = %d\n", &x, x);
+    printf("&p1 = %p, p1 = %p\n", &p1, p1);
+    printf("&pp1 = %p, pp1 = %p\n", &pp1, pp1);
+
+    printf("*p1 = %d\n", *p1);
+    printf("**pp1 = %d\n", **pp1);
+
+    printf("About to execute *p1 = 2...\n");
+    *p1 = 2;
+    printf("x = %d\n", x);
+
+    printf("About to execute **pp1 = 3...\n");
+    **pp1 = 3;
+    printf("x = %d\n", x);
+
+    const int y = 2;
+    const int *p2 = &y;
+    printf("*p2 = %d\n", *p2);
+
+    const int * const p3 = &x
+    //int *p2 = &y;
+    printf("*p3 = %d\n", *p3);
+    //p3 = &y;
+
+    //constexpr int cexpr = 3;
+    //int *p4 = &cexpr;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    pointersDemo1();
     return 0;
 }
