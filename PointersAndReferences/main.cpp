3a1d4f62e594e1295b26c5364bf05f4f4c40001b
migarn <michalgarnczarski@gmail.com>
2020-05-29 19:36:08 +0200

    Functions sumArr and pointersDemo2 added

--- PointersAndReferences/main.cpp
+++ PointersAndReferences/main.cpp
@@ -50,9 +50,56 @@ void referencesDemo()
     //double &* prx = &rx;
 }
 
+int sumArr(int (*p)[5])
+{
+   int sum = 0;
+   for (int i = 0; i < 5; i++) {
+      sum += (*p)[i]; // or sum += *((*p) + i);
+   }
+   return sum;
+}
+
+void pointersDemo2()
+{
+   int a[] = {1, 2, 3, 4, 5};
+   //int b[] = {1, 2, 3, 4, 5, 6};
+   int *p1 = a;
+
+   printf("&a = %p, a = %p, &a[0] = %p\n",&a, a, &a[0]);
+   printf("a[0] = %d, *p1 = %d\n", a[0], *p1);
+
+   //int (*p2)[] = &a; // !!!
+   //int (*p2)[5] = &b; // !!!
+   int (*p2)[5] = &a;
+   printf("&p2 = %p, p2 = %p, *p2 = %p, **p2 = %d, (*p2)[2] = %d\n",
+           &p2, p2, *p2, **p2, (*p2)[2]);
+
+   printf("sumArr(a) = %d\n", sumArr(&a));
+   //printf("sumArr(a) = %d\n", sumArr(&b)); // !!!
+
+   constexpr int asize = sizeof (a) / sizeof (int);
+   // Print all elements of "a"
+   for (int i = 0; i < asize; ++i) {
+      printf("a[%d] = %d\n", i, a[i]);
+   }
+
+   // The same but with the use of different notation
+   for (int i = 0; i < asize; ++i) {
+      printf("*(a + %d) = %d\n", i, *(a + i)); // a[i] = *(a + i)
+   }
+
+   // The same but this time with the use of a pointer
+   int *p3 = a;
+   for (int i = 0; i < asize; ++i) {//or for (int i = 0; i < asize; ++i, p3++)
+      printf("*p3 = %d\n", *p3);
+      p3++;
+   }
+}
+
 int main()
 {
     pointersDemo1();
     referencesDemo();
+    pointersDemo2();
     return 0;
 }
