aaad7b9430687ddba31f380850e19a6031dbab49
migarn <michalgarnczarski@gmail.com>
2020-05-29 20:12:32 +0200

    argByValue, argByReference and argByPointer functions added

--- PassingAgumentsToFunctions/main.cpp
+++ PassingAgumentsToFunctions/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 
-using namespace std;
+int argByValue(int i)
+{
+    return 2 * i;
+}
+
+int argByReference(int &r)
+{
+    r *= 2;
+    return 5 * r;
+}
+
+int argByPointer(int *p)
+{
+    *p *= 2;
+    return 5 * *p;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    using std::cout;
+    using std::endl;
+
+    int x = 1;
+    cout << "x = " << x << endl;
+
+    cout << "Calling argByValue(x):" << endl;
+    int y = argByValue(x);
+    cout << "x = " << x << "y = " << y << endl;
+
+    cout << "Calling argByReference(x):" << endl;
+    y = argByReference(x);
+    cout << "x = " << x << "y = " << y << endl;
+
+    cout << "Calling argByPointer(x):" << endl;
+    y = argByPointer(&x);
+    cout << "x = " << x << "y = " << y << endl;
+
     return 0;
 }
