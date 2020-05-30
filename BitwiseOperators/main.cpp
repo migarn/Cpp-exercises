60a28443bf7f98df24f3d91af83934fc4addb828
migarn <michalgarnczarski@gmail.com>
2020-05-30 18:33:20 +0200

    bitwiseOpsDemo2 function added

--- BitwiseOperators/main.cpp
+++ BitwiseOperators/main.cpp
@@ -19,8 +19,34 @@ void bitwiseOpsDemo1()
     printf("y >> 1 = %u (0x%x)\n", y >> 1, y >> 1);
 }
 
+void bitwiseOpsDemo2()
+{
+    using std::cout;
+    using std::endl;
+    using std::bitset;
+
+    unsigned int x = 5;
+    unsigned int y = 10;
+
+    constexpr auto sizeOfUInt = 8 * sizeof(unsigned int);
+
+    cout << "x = " << bitset<sizeOfUInt>(x) << endl;
+    cout << "y = " << bitset<sizeOfUInt>(x) << endl;
+
+    cout << "x || y = " << bitset<sizeOfUInt>(x || y) << endl;
+    cout << "x | y = " << bitset<sizeOfUInt>(x | y) << endl;
+    cout << "x && y = " << bitset<sizeOfUInt>(x && y) << endl;
+    cout << "x & y = " << bitset<sizeOfUInt>(x & y) << endl;
+    cout << "x ^ y = " << bitset<sizeOfUInt>(x ^ y) << endl;
+    cout << "~x = " << bitset<sizeOfUInt>(~x) << endl;
+
+    cout << "y << 1 = " << bitset<sizeOfUInt>(y << 1) << endl;
+    cout << "y >> 1 = " << bitset<sizeOfUInt>(y >> 1) << endl;
+}
+
 int main()
 {
     bitwiseOpsDemo1();
+    bitwiseOpsDemo2();
     return 0;
 }
