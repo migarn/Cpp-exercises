6409d2bac356b831489a907e2990b9718a84a781
migarn <michalgarnczarski@gmail.com>
2020-05-30 18:13:21 +0200

    bitwiseOpsDemo1 function added

--- BitwiseOperators/main.cpp
+++ BitwiseOperators/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <bitset>
 
-using namespace std;
+void bitwiseOpsDemo1()
+{
+    unsigned int x = 5;
+    unsigned int y = 10;
+
+    printf("x || y = %u (0x%x)\n", x || y, x || y);
+    printf("x | y = %u (0x%x)\n", x | y, x | y);
+
+    printf("x && y = %u (0x%x)\n", x && y, x && y);
+    printf("x & y = %u (0x%x)\n", x & y, x & y);
+
+    printf("x ^ y = %u (0x%x)\n", x ^ y, x ^ y);
+    printf("~x = %u (0x%x)\n", ~x, ~x);
+
+    printf("y << 1 = %u (0x%x)\n", y << 1, y << 1);
+    printf("y >> 1 = %u (0x%x)\n", y >> 1, y >> 1);
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    bitwiseOpsDemo1();
     return 0;
 }
