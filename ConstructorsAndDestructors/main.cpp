9238abc02e978b7b95e7d59d55414e52ae698e94
migarn <michalgarnczarski@gmail.com>
2020-06-01 12:38:01 +0200

    main class supplemented

--- ConstructorsAndDestructors/main.cpp
+++ ConstructorsAndDestructors/main.cpp
@@ -39,6 +39,55 @@ private:
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "About to execute Vector 2D v1{1,2}..." << endl;
+    Vector2D v1{1,2};
+    cout << endl;
+
+    cout << "About to execute Vector 2D v2{3,4}..." << endl;
+    Vector2D v2{3,4};
+    cout << endl;
+
+    cout << "About to execute cout << v2.length()..." << endl;
+    cout << "v2.length() = " << v2.length() << endl;
+
+    cout << "About to execute Vector 2D v3{v1}..." << endl;
+    Vector2D v3{v1};
+    cout << endl;
+
+    cout << "About to execute Vector 2D v4 = v2..." << endl;
+    Vector2D v4 = v2;
+    cout << endl;
+
+    cout << "About to execute Vector 2D v5 {}..." << endl;
+    Vector2D v5 {};
+    cout << endl;
+
+    cout << "About to execute v5 = v1..." << endl;
+    v5 = v1;
+    cout << endl;
+
+    cout << "About to execute Vector2D *pV2D = new Vector2D(10,11)..." << endl;
+    Vector2D *pV2D = new Vector2D(10,11);
+    cout << endl;
+
+    cout << "About to execute Vector2D *pArrV2D = new Vector2D[2]..." << endl;
+    Vector2D *pArrV2D = new Vector2D[2];
+    cout << endl;
+
+    cout << "About to execute delete pV2D..." << endl;
+    delete pV2D;
+    cout << endl;
+
+    cout << "About to execute delete [] pArrV2D..." << endl;
+    delete [] pArrV2D;
+    cout << endl;
+
+    {
+        cout << "About to execute auto uPtr = std::unique_ptr..." << endl;
+        auto uPtr = std::unique_ptr<Vector2D>(new Vector2D(10,11));
+        cout << endl;
+        cout << "About to leave the local scope..." << endl;
+    }
+
     return 0;
 }
