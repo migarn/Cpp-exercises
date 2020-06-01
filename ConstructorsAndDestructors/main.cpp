cd684cce25268d6da412aff01b4e79c1e2f4f843
migarn <michalgarnczarski@gmail.com>
2020-06-01 12:14:10 +0200

    Vector 2D class added

--- ConstructorsAndDestructors/main.cpp
+++ ConstructorsAndDestructors/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <cmath>
+#include <memory>
 
-using namespace std;
+using std::cout;
+using std::endl;
+
+class Vector2D
+{
+public:
+    Vector2D(double x = 0, double y = 0) : x_{x}, y_{y} {
+        cout << "...constructor working...";
+    }
+
+    Vector2D(const Vector2D &other) {
+        cout << "...copy constructor working...";
+        this->x_ = other.x_;
+        this->y_ = other.y_;
+    }
+
+    Vector2D& operator(const Vector2D &other) {
+        cout << "...copy operator= working...";
+        if (&other != this) {
+            this->x_ = other.x_;
+            this->y_ = other.y_;
+        }
+
+        return *this;
+    }
+
+    ~Vector2D() { cout << "...destructor working..." << endl; }
+
+    double length() const { return sqrt(x_ * x_ + y_ * y_); }
+
+private:
+    double x_;
+    double y_;
+};
 
 int main()
 {
