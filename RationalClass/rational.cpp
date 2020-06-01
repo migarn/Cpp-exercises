49455eac3712f3b171c7a439849a8156144c57c6
migarn <michalgarnczarski@gmail.com>
2020-06-01 15:02:05 +0200

    Operator= implemented

--- RationalClass/rational.cpp
+++ RationalClass/rational.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
-// sprawdzic, czy trzeba powyższe
+#include <iostream> // sprawdzic, czy trzeba powyższe
 
-Rational::Rational(int numerator, int denominator = 1) : numer_{numerator}, denom_{denominator} {};
-Rational::Rational(const Rational& copy) : numer_{copy.numer_}, denom_{copy.denom_} {};
+Rational::Rational(int numerator, int denominator = 1) : numer{numerator}, denom{denominator} {};
+Rational::Rational(const Rational& copy) : numer{copy.numer}, denom{copy.denom} {};
 
+Rational& Rational::operator=(const Rational& other) {
+    if (&other != this) {
+        this->numer = other.numer;
+        this->denom = other.denom;
+        this->gcdND = other.gcdND; // to do weryfikacji
+    }
+    return this;
+}
